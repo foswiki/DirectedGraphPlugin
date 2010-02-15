db3f14784ad2a7e535b477e4c7623f48f51be39a
GeorgeClark <GeorgeClark@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2010-02-15 21:17:10 +0000

    Item8495:  prepare DirectedGraphPlugin for Beta Release Candidate Item8526:  Didn't set version
    git-svn-id: http://svn.foswiki.org/trunk/DirectedGraphPlugin@6341 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Plugins/DirectedGraphPlugin/build.pl
+++ lib/Foswiki/Plugins/DirectedGraphPlugin/build.pl
@@ -11,7 +11,7 @@ $build = new Foswiki::Contrib::Build('DirectedGraphPlugin');
 # files, but be warned, they can be overridden at upload time!
 
 # name of web to upload to
-$build->{UPLOADTARGETWEB} = 'Extensions';
+$build->{UPLOADTARGETWEB} = 'Sandbox/Beta';
 
 # Full URL of pub directory
 $build->{UPLOADTARGETPUB} = 'http://foswiki.org/pub';
