de927013f12e5f67551b2d05834aedd11d7bbeb8
BryanThale <BryanThale@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2010-03-29 02:14:30 +0000

    Item8711: Bump release number and update change history
    git-svn-id: http://svn.foswiki.org/trunk/DirectedGraphPlugin@6986 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Plugins/DirectedGraphPlugin.pm
+++ lib/Foswiki/Plugins/DirectedGraphPlugin.pm
@@ -44,7 +44,7 @@ our $VERSION = '$Rev$';
 # # This is a free-form string you can use to "name" your own plugin version.
 # # It is *not* used by the build automation tools, but is reported as part
 # # of the version number in PLUGINDESCRIPTIONS.
-our $RELEASE = '1.7';
+our $RELEASE = '1.8';
 
 #
 # # Short description of this plugin
